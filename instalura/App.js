f1e0ad6382940972eaa43b309bc7c90599c6327a
gi-armani <giovana_armani@usp.br>
2021-01-17 19:18:31 -0300

    Projeto inicial

---
+++ instalura/App.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+      <Text>Giovana</Text>
+      <Text>Juliana</Text>
+    </Fragment>
+  );
+};
+
+export default App;
